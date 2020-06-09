101fb78c337c3b2066b14d12e3d7674d1ae3f2d3
Guilehrme <glrm1991@gmail.com>
2020-06-08 22:25:32 -0300

    RocketSeat - GoBarber - Modulo 2

--- Gostack/Gobarber/src/routes.js
+++ Gostack/Gobarber/src/routes.js
@@ -1,17 +1,30 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+// import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Guilherme Fazam',
-    email: 'glrm1991@gmail.com.br',
-    password_hash: '2wsx!Qaz',
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'Guilherme Fazam',
+//     email: 'glrm1991@gmail.com.br',
+//     password_hash: '2wsx!Qaz',
+//   });
+
+//   return res.json(user);
+// });
 
 export default routes;
