0507090a2e2a427f34aedd36804acd632f9565a0
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-07-06 17:59:00 -0300

    Rocketseat - Nível 2 - Iniciando back-end do app - Upload de imagens - Atualizando avatar

--- nivel-2/primeiro-projeto-node/src/routes/users.routes.ts
+++ nivel-2/primeiro-projeto-node/src/routes/users.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
-
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
-
 import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
-
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
@@ -35,8 +33,19 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        console.log(request.file);
-        return response.json({ ok: true });
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
     },
 );
 
