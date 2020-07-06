2598a1e7d026b4708d7632d55be16e7521acae45
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-07-06 15:31:07 -0300

    Rocketseat - Nível 2 - Iniciando back-end do app - Autenticação - Gerando token JWT

--- nivel-2/primeiro-projeto-node/src/services/AuthenticateUserService.ts
+++ nivel-2/primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -1,6 +1,6 @@
 import { getRepository } from 'typeorm';
-
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -33,11 +34,15 @@ class AuthenticateUserService {
         if (!passwordMatched) {
             throw new Error('Incorrect email password combination.');
         }
-
         // Usuario autenticado
+        const token = sign({}, 'c2e6a9650277011d9d3b116a46b14d8d', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
 
         return {
             user,
+            token,
         };
     }
 }
