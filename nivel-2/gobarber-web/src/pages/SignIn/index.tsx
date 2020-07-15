cd56289ebeaddabfa979b6f268f7f9806764c993
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-07-15 18:50:05 -0300

    Rocketseat - Nível 3 - Iniciando front-end web - Autenticação - Login pelo Contexto

--- nivel-2/gobarber-web/src/pages/SignIn/index.tsx
+++ nivel-2/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
+
+import { AuthContext } from '../../context/AuthContext';
 
 import getValidationErros from '../../utils/getValidationErros';
 import Input from '../../components/Input';
@@ -11,28 +13,42 @@ import Button from '../../components/Button';
 
 import logo from '../../assets/logo.svg';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const { signIn } = useContext(AuthContext);
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('E-mail inválido'),
-        password: Yup.string().required('Senha obrigatória'),
-      });
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      const error = getValidationErros(err);
-      formRef.current?.setErrors(error);
-    }
-  }, []);
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('E-mail inválido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const error = getValidationErros(err);
+        formRef.current?.setErrors(error);
+      }
+    },
+    [signIn],
+  );
 
   return (
     <Container>
