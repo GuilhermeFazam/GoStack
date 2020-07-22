fe04f2e0ea3b4a6fa5a886633dc72fb874d81f7e
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-07-22 16:38:22 -0300

    Rocketseat - Nível 3 - Iniciando Aplicativo Mobile - Autenticação e cadastro - Foco e preenchimento

--- nivel-3/appgobarber/src/components/Input/index.tsx
+++ nivel-3/appgobarber/src/components/Input/index.tsx
@@ -1,8 +1,10 @@
 import React, {
     useEffect,
+    useCallback,
     useRef,
     useImperativeHandle,
     forwardRef,
+    useState,
 } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
@@ -27,14 +29,26 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
 ) => {
     const inputElementRef = useRef<any>(null);
     const { registerField, defaultValue, fieldName, error } = useField(name);
-
     const inputValueRef = useRef<inputValueReference>({ value: defaultValue });
+
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+    }, []);
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+        setIsFilled(!!inputValueRef.current.value);
+    }, []);
 
     useImperativeHandle(ref, () => ({
         focus() {
             inputElementRef.current.focus();
         },
     }));
+
     useEffect(() => {
         registerField<string>({
             name: fieldName,
@@ -52,12 +66,18 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
     }, [fieldName, registerField]);
 
     return (
-        <Container>
-            <Icon name={icon} size={20} color="#666360" />
+        <Container isFocused={isFocused}>
+            <Icon
+                name={icon}
+                size={20}
+                color={isFocused || isFilled ? '#ff9000' : '#666360'}
+            />
             <TextInput
                 ref={inputElementRef}
                 keyboardAppearance="dark"
                 placeholderTextColor="#666360"
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
                 defaultValue={defaultValue}
                 onChangeText={value => {
                     inputValueRef.current.value = value;
--- nivel-3/appgobarber/src/components/Input/styles.ts
+++ nivel-3/appgobarber/src/components/Input/styles.ts
@@ -1,25 +1,35 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
+interface ContainerProps {
+    isFocused: boolean;
+}
 
-export const Container = styled.View`
-    width:100%;
-    height:60px;
+export const Container = styled.View<ContainerProps>`
+    width: 100%;
+    height: 60px;
     padding: 0 16px;
     background: #232129;
-    border-radius:10px;
+    border-radius: 10px;
+    border: 2px solid #232129;
     margin-bottom: 8px;
     flex-direction: row;
     align-items: center;
+
+    ${props =>
+        props.isFocused &&
+        css`
+            border-color: #ff9000;
+        `}
 `;
 
 export const TextInput = styled.TextInput`
-  flex:1;
-  color:#fff;
-  font-size: 16px;
-  font-family: 'RobotoSlab-Regular';
+    flex: 1;
+    color: #fff;
+    font-size: 16px;
+    font-family: 'RobotoSlab-Regular';
 `;
 
 export const Icon = styled(FeatherIcon)`
     margin-right: 16px;
-`
+`;
