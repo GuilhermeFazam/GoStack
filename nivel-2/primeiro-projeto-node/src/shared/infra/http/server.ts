2e2356de0474ed5dadc42676be3412598ddd4d31
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-07-27 17:39:15 -0300

    Rocketseat - Nível 4 - Arquitetura e testes no Node.js - Arquitetura e DDD - Configurando Imports

--- nivel-2/primeiro-projeto-node/src/shared/infra/http/server.ts
+++ nivel-2/primeiro-projeto-node/src/shared/infra/http/server.ts
@@ -1,12 +1,11 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
-import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
-
-import './database';
+import 'express-async-errors';
+import routes from '@shared/infra/http/routes';
+import '@shared/infra/typeorm';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -15,6 +14,7 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    console.log(err);
     if (err instanceof AppError) {
         return response.status(err.statusCode).json({
             status: 'error',
