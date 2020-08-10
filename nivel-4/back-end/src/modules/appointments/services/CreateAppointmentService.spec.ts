33d92ad6705b22fe8253e4ec408c6e904b337c25
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-10 16:30:06 -0300

    Rocketseat - Nível 4 - Arquitetura e testes no Node.js - Testes e TDD - Criando o primeiro teste

--- nivel-4/back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel-4/back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,21 @@
-import CreateAppointmentServicespec from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-    it('shold be able to create a new appointment', () => {
-        expect(1 + 2).toBe(3);
+    it('shold be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = createAppointment.execute({
+            date: new Date(),
+            provider_id: '123123213',
+        });
+
+        expect(await appointment).toHaveProperty('id');
+        expect((await appointment).provider_id).toBe('123123213');
     });
 
     // it('Shold not be able to create tow appointments on the same time', () => {
