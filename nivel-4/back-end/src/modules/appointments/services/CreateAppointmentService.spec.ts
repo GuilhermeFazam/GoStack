e6a3ed10115be3f4ffe098aca7b6e9c623c865ae
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-10 15:41:52 -0300

    Rocketseat - Nível 4 - Arquitetura e testes no Node.js - Testes e TDD - Pensando nos testes

--- nivel-4/back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel-4/back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum tow numbers', () => {
-    expect(1 + 2).toBe(3);
+import CreateAppointmentServicespec from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+    it('shold be able to create a new appointment', () => {
+        expect(1 + 2).toBe(3);
+    });
+
+    // it('Shold not be able to create tow appointments on the same time', () => {
+    //     expect(1 + 2).toBe(3);
+    // });
 });
