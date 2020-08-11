247d2f1d976bd0ee3ca4bb3fb0ebd611894a5e83
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-11 12:31:55 -0300

    Rocketseat - Nível 4 - Arquitetura e testes no Node.js - Testes e TDD - Provider de storage

--- nivel-4/back-end/src/modules/users/services/UpdateUserAvatarService.ts
+++ nivel-4/back-end/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,8 @@
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
-import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface IRequest {
     user_id: string;
@@ -15,6 +13,9 @@ class UpdateUserAvatarService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUserRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
     ) {}
 
     public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -28,20 +29,12 @@ class UpdateUserAvatarService {
         }
 
         if (user.avatar) {
-            const userAvatarFilePath = path.join(
-                uploadConfig.directory,
-                user.avatar,
-            );
-            const userAvatarFileExist = await fs.promises.stat(
-                userAvatarFilePath,
-            );
-
-            if (userAvatarFileExist) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
 
-        user.avatar = avatarFilename;
+        const fileName = await this.storageProvider.saveFile(avatarFilename);
+
+        user.avatar = fileName;
 
         await this.usersRepository.save(user);
 
