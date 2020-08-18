e2e29a84415893a1112d9d81c6046600d1fe4ad1
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-18 18:40:03 -0300

    Rocketseat - Nível 4 - Continuando back-end do app - Agendamento - Listando dias disponíveis

--- nivel-4/back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ nivel-4/back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -15,19 +15,44 @@ describe('ListProvidersService', () => {
     it('should be able to list the the month avalability from provider', async () => {
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
-            date: new Date(2020, 3, 20, 8, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
             date: new Date(2020, 4, 20, 8, 0, 0),
         });
-
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 9, 0, 0),
+        });
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
             date: new Date(2020, 4, 20, 10, 0, 0),
         });
-
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 11, 0, 0),
+        });
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 12, 0, 0),
+        });
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 13, 0, 0),
+        });
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 14, 0, 0),
+        });
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 15, 0, 0),
+        });
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 16, 0, 0),
+        });
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 17, 0, 0),
+        });
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
             date: new Date(2020, 4, 21, 8, 0, 0),
@@ -43,7 +68,7 @@ describe('ListProvidersService', () => {
             expect.arrayContaining([
                 { day: 19, avaliable: true },
                 { day: 20, avaliable: false },
-                { day: 21, avaliable: false },
+                { day: 21, avaliable: true },
                 { day: 22, avaliable: true },
             ]),
         );
