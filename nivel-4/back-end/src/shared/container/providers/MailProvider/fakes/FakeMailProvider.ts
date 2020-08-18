798f9e554ee164cc6af9771cd7c5ccc22dd301e7
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-17 23:07:01 -0300

    Rocketseat - Nível 4 - Continuando back-end do app - Perfil do usuário - Template de emails

--- nivel-4/back-end/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ nivel-4/back-end/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,17 +1,10 @@
 import IStorageProvider from '../models/IMailProvider';
-
-interface Message {
-    to: string;
-    body: string;
-}
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 export default class FakeMailProvider implements IStorageProvider {
-    private messages: Message[] = [];
+    private messages: ISendMailDTO[] = [];
 
-    public async sendMail(to: string, body: string): Promise<void> {
-        this.messages.push({
-            to,
-            body,
-        });
+    public async sendMail(message: ISendMailDTO): Promise<void> {
+        this.messages.push(message);
     }
 }
