9fe8b2bd66d1beba4e5ad595f33d93aa1e236bae
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-19 15:29:27 -0300

    Rocketseat - Nível 4 - Continuando back-end do app - Agendamento - Excluindo horários antigos

--- nivel-4/back-end/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ nivel-4/back-end/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -12,14 +12,18 @@ describe('ListProvidersService', () => {
         );
     });
 
-    it('should be able to list the the month avalability from provider', async () => {
+    it('should be able to list the the day avalability from provider', async () => {
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
-            date: new Date(2020, 4, 20, 8, 0, 0),
+            date: new Date(2020, 4, 20, 14, 0, 0),
         });
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
-            date: new Date(2020, 4, 20, 10, 0, 0),
+            date: new Date(2020, 4, 20, 15, 0, 0),
+        });
+
+        jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+            return new Date(2020, 4, 20, 11).getTime();
         });
 
         const availability = await listProviderDayAvailability.execute({
@@ -32,9 +36,12 @@ describe('ListProvidersService', () => {
         expect(availability).toEqual(
             expect.arrayContaining([
                 { hour: 8, avaliable: false },
-                { hour: 9, avaliable: true },
+                { hour: 9, avaliable: false },
                 { hour: 10, avaliable: false },
-                { hour: 11, avaliable: true },
+                { hour: 13, avaliable: true },
+                { hour: 14, avaliable: false },
+                { hour: 15, avaliable: false },
+                { hour: 16, avaliable: true },
             ]),
         );
     });
