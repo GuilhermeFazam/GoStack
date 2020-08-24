c9b644842d04d137b8bec0fd191af6f54d2e7097
Guilherme Althman Fazam <guilherme.fazam@neoris.com>
2020-08-24 20:01:19 -0300

    Rocketseat - Nível 4 - Finalizando back-end do app - Prestador de serviços - Enviando notificações

--- nivel-4/back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ nivel-4/back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,16 +1,9 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-
-// SOLID
-
-// # Single Responsability Principle
-// Open Closed  Principle
-// # Liskov Substitution Principle
-// Interface Segregation Principle
-// # Dependence Invertion Principle
+import INotificationRepository from '@modules/notifications/repositories/INotificationRepository';
 
 interface IRequest {
     provider_id: string;
@@ -23,6 +16,9 @@ class CreateAppointmentService {
     constructor(
         @inject('AppointmentsRepository')
         private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('NotificationsRepository')
+        private notificationsRepository: INotificationRepository,
     ) {}
 
     public async execute({
@@ -61,6 +57,15 @@ class CreateAppointmentService {
             provider_id,
             user_id,
             date: appointmentDate,
+        });
+        const dateFormatted = format(
+            appointmentDate,
+            "dd/MM/yyyy 'às' HH:mm'h'",
+        );
+
+        await this.notificationsRepository.create({
+            recipient_id: provider_id,
+            content: `Novo agendamento para dia ${dateFormatted}`,
         });
 
         return appointment;
